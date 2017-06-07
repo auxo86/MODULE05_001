81b5a8b7c24cc6f35a3e922c02e7f27f003b77c4
van chung chen <auxo_86@hotmail.com>
2017-06-07 19:17:36 +0800

    所以Web.config ==> <customErrors mode="On"></customErrors>要記得開，除厝網頁可以自訂。用attribute, [HandleError(View = "AA"), 一樣要放在Shared目錄下面。

--- MODULE05_001/Controllers/HomeController.cs
+++ MODULE05_001/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace MODULE05_001.Controllers
 {
+    //用AA.cshtml自訂錯誤網頁，範圍是全controller
     //[HandleError(View = "AA")] 
     public class HomeController : Controller
     {
@@ -16,6 +17,8 @@ namespace MODULE05_001.Controllers
         }
 
         //Web.config ==> <customErrors mode="On"></customErrors>
+        //上面這行一定要啟用，否則沒有自訂除錯的功能
+        //下面這個標籤限定只有這個action有自訂除錯顯示，上面的index 出錯可沒人理了。然後Shared資料夾是固定的，Error.cshtml則是固定的除錯訊息顯示頁
         //[HandleError]
         public ActionResult ExceptionDemo()
         {
